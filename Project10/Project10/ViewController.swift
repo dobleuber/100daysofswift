4d0f03c47990f4fda59d948e29a3d2224764ba39
Wbert Adrian Castro Vera <wbertc@gmail.com>
2020-01-04 16:48:59 -0500

    Project 10 part 1

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Wbert Castro on 1/4/20.
+//  Copyright © 2020 Wbert Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue a PersonCell.")
+        }
+        return cell
+    }
+}
+
