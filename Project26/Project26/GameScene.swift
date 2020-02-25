23dcd68da1fe9bf349d68db0fe7947e0d963815c
Wbert Adrian Castro Vera <wbertc@gmail.com>
2020-02-24 21:01:54 -0500

    Day 85 another game, this time we are gonna accelerometer!!

---
+++ Project26/Project26/GameScene.swift
@@ -0,0 +1,104 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by Wbert Castro on 2/24/20.
+//  Copyright © 2020 Wbert Castro. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case vortex = 8
+    case finish = 16
+}
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        loadBackground()
+        loadLevel()
+    }
+    
+    func loadBackground() {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+    }
+    
+    func loadLevel() {
+        guard let levelUrl = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
+            fatalError("Could not find level1.txt in the app bundle.")
+        }
+        
+        guard let levelString = try? String(contentsOf: levelUrl) else {
+            fatalError("Could not load level1.txt from the app bundle.")
+        }
+        
+        let lines = levelString.components(separatedBy: "\n").reversed()
+        
+        for (row, line) in lines.enumerated() {
+            for (column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                switch letter {
+                case "x":
+                    // load wall
+                    let node = SKSpriteNode(imageNamed: "block")
+                    node.position = position
+                    
+                    node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+                    node.physicsBody?.isDynamic = false
+                    
+                    addChild(node)
+                    
+                case "v":
+                    // load vortex
+                    let node = SKSpriteNode(imageNamed: "vortex")
+                    node.name = "vortex"
+                    node.position = position
+                    node.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 1)))
+                    
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.vortex.rawValue
+                    node.physicsBody?.isDynamic = false
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    addChild(node)
+                case "s":
+                    // load start
+                    let node = SKSpriteNode(imageNamed: "star")
+                    node.position = position
+                    node.name = "star"
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+                    node.physicsBody?.categoryBitMask = CollisionTypes.star.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    addChild(node)
+                case "f":
+                    // load finish point
+                    let node = SKSpriteNode(imageNamed: "finish")
+                    node.position = position
+                    node.name = "finish"
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+                    node.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    addChild(node)
+                case " ":
+                    continue
+                default:
+                    fatalError("Unknown level letter: \(letter)")
+                }
+            }
+        }
+    }
+}
